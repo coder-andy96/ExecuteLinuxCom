8ee9db27342c1ffb094475f33e87ffbc2368b8bd
逸然丶诺 <50472547+nuonuo96@users.noreply.github.com>
2019-07-29 16:13:35 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+#include"ExecuteLinuxCom.h"
 
+int main()
+{
+  char *command;
+  int iter=0;
+  command=(char*)malloc(MAX+1);
+  chdir("/home/");
+  while(1){
+    iter=0;
+      signal_set();
+    print_prompt();
+    scan_command();
+    parse_semicolon(command);
+    
+    while(all[iter!=NULL])
+    {
+      execute(all[iter]);
+      iter+=1;
+    }
+  }
+}
